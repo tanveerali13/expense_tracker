e4b0e206a85c2416fe8ec02ff594458d51a00167
tanveerali13 <tanveer.nsu13@gmail.com>
2023-09-21 11:02:29 -0400

    Fixed date update

--- app/edit.php
+++ app/edit.php
@@ -6,10 +6,10 @@ error_reporting(E_ALL);
 
 require_once "_includes/db_connect.php";
 
-$query = "UPDATE expense SET expense_name=?, amount=?, details=? WHERE id=?";
+$query = "UPDATE expense SET added_on=?, expense_name=?, amount=?, details=? WHERE id=?";
 
 if ($stmt = mysqli_prepare($link, $query)) {
-    mysqli_stmt_bind_param($stmt, 'sisi', $_GET["expense_name"], $_GET["amount"], $_GET["details"], $_GET["id"]);
+    mysqli_stmt_bind_param($stmt, 'ssisi', $_GET["date"], $_GET["expense_name"], $_GET["amount"], $_GET["details"], $_GET["id"]);
     mysqli_stmt_execute($stmt);
     $stmt->close();
 }
